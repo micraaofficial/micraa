3a8babab50686435feab87a2da2b16e02ef328d7
micraaofficial <micraaofficial@gmail.com>
2026-03-07 14:59:35 +0400

    update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Micraa Test</title>
+</head>
+<body>
+
+<h1>Micraa TEST 123</h1>
+
+<p>If you see this text, SFTP auto upload is working.</p>
+
+<a href="/services">Browse Services</a>
+
+</body>
+</html>
